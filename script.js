c0d133a7f1cc72ffbc97978f0b422f4639301573
Apmin5 <apmin55555@gmail.com>
2022-01-05 22:49:53 +0630

    hdbpro2

--- script.js
+++ script.js
@@ -3,4 +3,26 @@ const disNon = document.querySelector('.dis-non');
 
 btn.addEventListener("click",()=>{
     disNon.style.display = "block";
+    btn.style.display = "none";
 })
+
+// const Header = document.querySelector(".Header");
+// const divOne = document.querySelector(".one");
+
+// const letter = "HAPPY";
+//     const letter1="BIRTH";
+//     const letter2 = "DAY";
+//     const chit = "CHITLAY";
+    
+// const splitLet = (leter)=>{
+//     return leter.split('');
+// }
+
+// const H1 = document.createElement("h1");
+
+// const lett = splitLet(letter).map((lett)=>{
+//     return lett;
+// })
+// H1.append(lett);
+// divOne.append(H1);
+// console.log(lett);
